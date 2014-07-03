e9ff56ab2a4a6853c2911d70be35945b366f5833
Matthew Martin <phy1729@gmail.com>
2014-07-03 18:24:57 -0500

    Correctly handle /me

--- src/io/github/dead_i/bungeerelay/listeners/ChatListener.java
+++ src/io/github/dead_i/bungeerelay/listeners/ChatListener.java
@@ -18,18 +18,15 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onChat(ChatEvent event) {
         if (!IRC.sock.isConnected()) return;
-        ProxiedPlayer p = (ProxiedPlayer) event.getSender();
+        ProxiedPlayer player = (ProxiedPlayer) event.getSender();
         String msg = event.getMessage();
-        if (!msg.startsWith("/") || (msg.startsWith("/me") && p.hasPermission("irc.me"))) {
-            String chanconf = IRC.config.getString("server.channel");
-            String channel = chanconf;
-            if (channel.isEmpty()) channel = IRC.config.getString("server.chanprefix") + p.getServer().getInfo().getName();
-            if (msg.startsWith("/me ")) msg = (char) 1 + "ACTION " + msg;
-            IRC.out.println(":" + IRC.uids.get(p) + " PRIVMSG " + channel + " :" + msg);
+        if (!msg.startsWith("/") || (msg.startsWith("/me "))) {
+            if (msg.startsWith("/me ")) msg = "\001ACTION " + msg.substring(4) + "\001";
+            IRC.out.println(":" + IRC.uids.get(player) + " PRIVMSG " + IRC.channel + " :" + msg);
 
-            if (!chanconf.isEmpty()) for (ProxiedPlayer o : plugin.getProxy().getPlayers()) {
-                if (!p.getServer().getInfo().getName().equals(o.getServer().getInfo().getName())) o.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', IRC.config.getString("formats.msg"))
-                        .replace("{SENDER}", p.getName())
+            for (ProxiedPlayer o : plugin.getProxy().getPlayers()) {
+                if (!player.getServer().getInfo().getName().equals(o.getServer().getInfo().getName())) o.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', IRC.config.getString("formats.msg"))
+                        .replace("{SENDER}", player.getName())
                         .replace("{MESSAGE}", msg)));
             }
         }
